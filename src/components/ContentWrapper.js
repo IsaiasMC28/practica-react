4318245298aeea89624628affc2aafdc6fba9159
IsaiasMC28 <14540@virtual.utsc.edu.mx>
2022-02-17 17:03:29 -0600

    practica react

---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,19 @@
+import TopBar from "./TopBar";
+import Footer from "./Footer";
+import ContentRowTop from "./ContentRowTop";
+
+const ContentWrapper = () => {
+  return (
+    <>
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+          <TopBar />
+          <ContentRowTop />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default ContentWrapper;
